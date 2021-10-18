a37389bfb7caac36c78d408bb89abc893e743284
Hoa Nguyen <hoant20199@google.com>
2021-10-18 18:37:45 +0700

    NTH

---
+++ app/src/main/java/com/example/android_theflashriderremake/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.android_theflashriderremake.Common;
+
+import com.example.android_theflashriderremake.Model.RiderModel;
+
+public class Common {
+    public static final String DRIVER_INFO_REFERENCE = "Riders";
+    public static RiderModel currentUser;
+}
